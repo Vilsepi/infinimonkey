c87dc6f43af689607a51e80e61eb4733576db7c7
Ville Seppanen <ville.seppanen@gofore.com>
2017-05-20 10:30:22 +0300

    Add ramblings api

---
+++ backend/ramblings-api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import json
+import boto3
+
+table = boto3.resource("dynamodb").Table(os.environ.get("RAMBLINGS_TABLE_NAME"))
+
+def _log_dynamo(response):
+    print("HTTPStatusCode:{}, RetryAttempts:{}, ScannedCount:{}, Count:{}".format(
+        response.get("ResponseMetadata").get("HTTPStatusCode"),
+        response.get("ResponseMetadata").get("RetryAttempts"),
+        response.get("ScannedCount"),
+        response.get("Count")
+    ))
+
+def get_ramblings(event, context):
+    response = table.scan(Limit=10)
+    _log_dynamo(response)
+    return {
+        "statusCode": 200,
+        "body": json.dumps(response["Items"], indent=1),
+        "headers": {"Access-Control-Allow-Origin": "*"}
+    }
