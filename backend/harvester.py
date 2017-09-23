d64fd52f2551bd58e56f973a1aa0d1a02130d33e
Ville Seppanen <ville.seppanen@gofore.com>
2017-09-23 10:14:47 +0300

    Add initial harvester

---
+++ backend/harvester.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+from xml.etree import ElementTree
+import json
+
+# Import vendor modules from a subdirectory
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "./vendored"))
+
+import requests
+from bs4 import BeautifulSoup
+import boto3
+
+
+# Download RSS feed and parse news entries
+def _get_feed_xml():
+    url = "http://feeds.feedburner.com/ampparit-uutiset"
+    response = requests.get(url)
+
+    ns = {"atom": "http://www.w3.org/2005/Atom"}
+    feed = ElementTree.fromstring(response.content)
+    feed_updated = feed.find("atom:updated", ns).text
+
+    id_prefix = "http://www.ampparit.com/redir.php?id="
+    entries = []
+    for entry in feed.findall("atom:entry", ns):
+        entries.append({
+            "id": entry.find("atom:id", ns).text.split(id_prefix, 1)[1],
+            "title": entry.find("atom:title", ns).text,
+            "updated": entry.find("atom:updated", ns).text,
+            "feed_url": entry.find("atom:link", ns).attrib.get("href"),
+            "author": entry.find("atom:author", ns).find("atom:name", ns).text
+        })
+    print("Parsed {} items".format(len(entries)))
+    return entries
+
+
+# Scrape given html to plaintext
+def _convert_html_to_text(html, source):
+    soup = BeautifulSoup(html, "lxml")
+    print("Parsing content from source " + source)
+
+    text = ""
+    if source == "Tivi":
+        for e in soup.find("div", class_="article-body").find_all("p", recursive=False):
+            text += e.get_text() + " "
+    elif source == "Yle":
+        for e in soup.find("div", class_="yle__article__content").find_all("p", recursive=False):
+            text += e.get_text() + " "
+    elif source == "Aamulehti":
+        for e in soup.find("div", class_="content--main").find_all("p", recursive=False):
+            text += e.get_text() + " "
+    elif source == "Ilta-Sanomat":
+        mess = soup.find("div", class_="body")
+        for script in mess.find_all("script"):
+            script.decompose()
+        for script in mess.select(".hidden"):
+            script.decompose()
+        text = mess.get_text()
+    else:
+        print("Fallback to crude parser")
+        for e in soup.find_all("p"):
+            text += e.get_text() + " "
+    return text
+
+
+def _get_content(item):
+    response = requests.get(item["feed_url"])
+    if response.status_code == 404:
+        print("Feed link is stale")
+        return None
+    else:
+        print("Fetched {} bytes of HTML from {}".format(len(response.content), response.url))
+        item["content_url"] = response.url
+        item["content"] = _convert_html_to_text(response.text, item["author"])
+
+        print(json.dumps(item, indent=1))
+        return item
+
+
+# Save entries to DynamoDB
+def _save_to_dynamo(items):
+    table_name = os.environ.get("CORPUS_TABLE_NAME")
+    table = boto3.resource("dynamodb").Table(table_name)
+    with table.batch_writer() as batch:
+        for item in items:
+            batch.put_item(Item=item)
+
+
+# Lambda entry point
+def handler(event, context):
+    print(event)
+    print(context)
+    print("Fetching feed")
+    headlines = _get_feed_xml()
+    print("Fetching content")
+    corpus_items = list(filter(None.__ne__, [_get_content(headline) for headline in headlines]))
+
+    if not event.get("is_local_dev"):
+        _save_to_dynamo(corpus_items[:10])
+    else:
+        print(json.dumps(headlines, indent=1))
+        raise NotImplementedError("Local DynamoDB usage not implemented")
+
+
+# Main function for local testing
+if __name__ == "__main__":
+    print("Local execution is not completely supported. Please run this in AWS Lambda.")
+    handler({"is_local_dev": True}, {})
